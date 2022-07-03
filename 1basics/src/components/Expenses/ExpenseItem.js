220aa41a488693ff4a6d9e71c18c2578ed6fda3b
Shaik Haseeb Ur Rahman <haseebshaik00@gmail.com>
2022-07-04 00:41:57 +0530

    useState done

--- 1basics/src/components/Expenses/ExpenseItem.js
+++ 1basics/src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,25 @@
+import { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 export default function ExpenseItem(props){
 
+    const [title, setTitle] = useState(props.expenseTitle);
+
+    const titleHandler = () => {
+        setTitle("clicked!");
+        console.log(title);
+    }
+
     return(
         <Card className="expense-item">
             <ExpenseDate expenseDate={props.expenseDate}/>
             <div className="expense-item__description">
-                <h2>{props.expenseTitle}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">$ {props.expensePrice}/-</div>
             </div>
+            <button onClick={titleHandler}>Click</button>
         </Card>
     );
 
